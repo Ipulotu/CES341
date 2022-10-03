ba8526dbbca3d7ad374ec7d3f51c1769913acfd4
ipulotu <ipulotu@byui.edu>
2022-10-03 12:35:00 -0600

    fixing two

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
     description: 'This API is running on node.js and is connected to a MongoDB with Mongodb Atlas',
   },
   host: 'ipulotu-ces341.onrender.com',
-  schemes: ['http'],
+  schemes: ['https'],
 };
 
 const outputFile = './swagger.json';
