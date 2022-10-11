a85ac2aa54036ede624454a4dedf29af7c899317
ipulotu <ipulotu@byui.edu>
2022-10-11 12:00:54 -0600

    Converting to ts

--- CookBook-API/routes/cookBook.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const CookBookRoutes = require('express').Router();
-const cookBook = require("../controllers/recipes");
-CookBookRoutes.get('/', cookBook.getAllRecipe);
-CookBookRoutes.get('/:id', cookBook.getRecipe);
-CookBookRoutes.post('/', cookBook.createRecipe);
-CookBookRoutes.delete('/:id', cookBook.deleteRecipe);
-module.exports = CookBookRoutes;
--- CookBook-API/routes/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const express = require('express');
-const router = express.Router();
-router.use('/', require('./swagger'));
-router.use('/cookbook', require('./cookBook'));
-module.exports = router;
