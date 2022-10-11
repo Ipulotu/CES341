00e8c23a60f4f8c686866c6be9f077cd3ab68d2e
ipulotu <ipulotu@byui.edu>
2022-10-11 09:58:00 -0600

    adding get id rount

--- CookBook-API/routes/cookBook.js
+++ CookBook-API/routes/cookBook.js
@@ -2,5 +2,6 @@
 const CookBookRoutes = require('express').Router();
 const cookBook = require("../controllers/recipes");
 CookBookRoutes.get('/', cookBook.getAllRecipe);
+CookBookRoutes.get('/:id', cookBook.getRecipe);
 CookBookRoutes.post('/', cookBook.createRecipe);
 module.exports = CookBookRoutes;
--- CookBook-API/routes/cookBook.ts
+++ CookBook-API/routes/cookBook.ts
@@ -1,11 +1,9 @@
 const CookBookRoutes = require('express').Router();
 
-
 const cookBook = require("../controllers/recipes");
 
-
 CookBookRoutes.get('/',  cookBook.getAllRecipe);
-
+CookBookRoutes.get('/:id',  cookBook.getRecipe);
 CookBookRoutes.post('/', cookBook.createRecipe);
 
 
