c416f80ef2e9e0ded4238b5b8bd9dd943ea520cf
ipulotu <ipulotu@byui.edu>
2022-10-17 15:06:35 -0600

    fixng put route

--- CookBook-API/routes/cookBook.ts
+++ CookBook-API/routes/cookBook.ts
@@ -4,7 +4,7 @@ const cookBook = require("../controllers/recipes");
 CookBookRoutes.get('/',  cookBook.getAllRecipe);
 CookBookRoutes.get('/:id',  cookBook.getRecipe);
 CookBookRoutes.post('/', cookBook.createRecipe);
-CookBookRoutes.put('/', cookBook.updateRecipe);
+CookBookRoutes.put('/:id', cookBook.updateRecipe);
 CookBookRoutes.delete('/:id', cookBook.deleteRecipe);
 
 module.exports = CookBookRoutes
