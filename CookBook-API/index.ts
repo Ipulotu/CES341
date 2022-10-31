e72ba07a7352a7d530a9d08ab3cfdfd2fb517265
ipulotu <ipulotu@byui.edu>
2022-10-31 15:36:27 -0600

    reverting code

--- CookBook-API/index.ts
+++ CookBook-API/index.ts
@@ -62,30 +62,37 @@ passport.use(new GoogleStrategy({
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: process.env.REDIRECT_URI
   },
-    async function(accessToken: null, refreshToken: null, profile:any, done:any) {
-      const connection = await mongodb.getDb().db("CookBook").collection('Recipes').find({ googleId: profile.id});
-      // const User = connection.model('User', UserSchema);
-      var user = connection.find({ googleId: profile.id});
-
-      //Cheking for user in db
-        if (!user) {
-          user = {
-            googleId: profile.id,
-            displayName: profile.displayName
-          };
-
-          //Adding user to db
-          connection.insertOne(user)    
-            .then((result: any) => {
-              console.log(result)
-            })
-          .catch((error: any) => console.error(error))
-
-        }else{
-          return done(null, user);
-        }
-    }
+  function(accessToken: null, refreshToken: null, profile:any, done:any) {
+    userProfile=profile;
+    return done(null, userProfile);
+}
 ));
+//     async function(accessToken: null, refreshToken: null, profile:any, done:any) {
+//       const connection = await mongodb.getDb().db("CookBook").collection('Recipes').find({ googleId: profile.id});
+//       // const User = connection.model('User', UserSchema);
+//       var user = connection.find({ googleId: profile.id});
+//       console.log(user);
+
+//       //Cheking for user in db
+//         if (!user) {
+//           user = {
+//             googleId: profile.id,
+//             displayName: profile.displayName
+//           };
+
+//           //Adding user to db
+//           connection.insertOne(user)    
+//             .then((result: any) => {
+//               console.log(result)
+//             })
+//           .catch((error: any) => console.error(error))
+
+//         }else{
+//           return done(null, user);
+//         }
+//     }
+// ));
+
 
 
 app.get('/auth/google', 
@@ -103,22 +110,6 @@ app.get('/auth/google/callback',
 
 
 
-
-        // The Google account has not logged in to this app before.  Create a
-        // new user record and link it to the Google account.
-
-    //   } else {
-    //     // The Google account has previously logged in to the app.  Get the
-    //     // user record linked to the Google account and log the user in.
-    //     db.get('SELECT * FROM users WHERE id = ?', [ cred.user_id ], function(err, user) {
-    //       if (err) { return cb(err); }
-    //       if (!user) { return cb(null, false); }
-    //       return cb(null, user);
-    //     });
-    //   }
-    // };
-
- 
 
 
 
